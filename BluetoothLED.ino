bf674a5e3e46524006461a6650ca3e40ec73b0c3
Manuel J. Caroca Gonzalez <manuelcarok@gmail.com>
2018-05-21 01:51:04 -0400

    Se crea proyecto de bluetooth chat LED

---
+++ BluetoothLED.ino
@@ -0,0 +1,44 @@
+#include <SoftwareSerial.h>
+ 
+// Initializing communication ports
+SoftwareSerial Bluetooth(11, 10); // TX/RX pins
+
+const int ledPIN = 9;
+ 
+void setup()  
+{
+  Bluetooth.begin(9600);
+  pinMode(ledPIN , OUTPUT);  //definir pin como salida
+}
+ 
+String getMessage(){
+  String msg = "";
+  char m;
+  
+  while(Bluetooth.available()) {
+      m = Bluetooth.read();
+      msg+=String(a);
+  }
+  return msg;
+}
+ 
+void loop()
+{
+    // Check if a message has been received
+    String msg = getMessage();
+    if(msg!=""){
+      if(msg == "encender"){
+        digitalWrite(ledPIN , HIGH);   // poner el Pin en HIGH
+        Bluetooth.println("LED encendida");
+        delay(100);
+      }
+      if(msg == "apagar"){
+        digitalWrite(ledPIN , LOW);   // poner el Pin en HIGH
+        Bluetooth.println("LED apagada");
+        delay(100);
+      }
+    } 
+    delay(10);
+}
+
+
